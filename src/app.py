783d11e56689c17fc2a8fcc5fb4b1d7eeaeefa53
Larry Botha <lawrenceguy@gmail.com>
2024-02-11 23:18:02 +0200

    feat(assets): WIP - bundle assets via flask

--- src/app.py
+++ src/app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flask_assets import Bundle, Environment
 
 app = Flask(__name__)
+
+assets = Environment(app)
+css = Bundle("./assets/styles/main.css", output="assets/dist/main.css")
+js = Bundle("./assets/scripts/*.js", output="assets/dist/main.js")
+
+assets.register("css", css)
+assets.register("js", js)
+css.build()
+js.build()
 
 
 @app.route("/")
