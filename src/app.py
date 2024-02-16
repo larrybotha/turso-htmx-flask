d596294fef3c3147426aa7ff1a04b38a4b8b8575
Larry Botha <lawrenceguy@gmail.com>
2024-02-16 21:54:24 +0200

    fix(assets): render from directory structure that flask expects

--- src/app.py
+++ src/app.py
@@ -4,8 +4,8 @@ from flask_assets import Bundle, Environment
 app = Flask(__name__)
 
 assets = Environment(app)
-css = Bundle("./assets/styles/main.css", output="assets/dist/main.css")
-js = Bundle("./assets/scripts/*.js", output="assets/dist/main.js")
+css = Bundle("./styles/main.css", output="dist/main.css")
+js = Bundle("./scripts/*.js", output="dist/main.js")
 
 assets.register("css", css)
 assets.register("js", js)
