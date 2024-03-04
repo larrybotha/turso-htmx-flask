f0ad1b4f613f1774dd7d8905123e9ade6819044c
Larry Botha <lawrenceguy@gmail.com>
2024-03-04 07:59:27 +0200

    refactor(home.py): simplify view

--- src/route_handlers/home.py
+++ src/route_handlers/home.py
@@ -8,114 +8,88 @@ from ..models import Link, User
 from ..utils.time import get_current_time_in_seconds
 
 
+def _handle_post(db_engine: Engine, /):
+    current_time_in_seconds = get_current_time_in_seconds()
+    user_name = request.form["user_name"]
+    email = request.form["email"]
+    full_name = request.form["full_name"]
+    github = request.form["github"]
+    twitter = request.form["twitter"]
+    youtube = request.form["youtube"]
+    linkedin = request.form["linkedin"]
+    facebook = request.form["facebook"]
+    validations = [
+        (user_name, "Username is required"),
+        (email, "Email is required"),
+        (full_name, "Your full name is required"),
+        (
+            any((github, twitter, youtube, linkedin, facebook)),
+            "Please select at least one social link",
+        ),
+    ]
+
+    for value, message in validations:
+        if not value:
+            return f'<div class="error">{message}</div>'
+
+    configs = [
+        (github, "github"),
+        (twitter, "twitter"),
+        (youtube, "youtube"),
+        (linkedin, "linkedin"),
+        (facebook, "facebook"),
+    ]
+
+    with Session(db_engine) as session:
+        user_id = str(uuid.uuid4())
+        delete_id = str(uuid.uuid4())
+        links = [
+            Link(
+                created_at=current_time_in_seconds,
+                id=str(uuid.uuid4()),
+                link=link,
+                user_id=user_id,
+                website=name,
+            )
+            for link, name in configs
+        ]
+        user = User(
+            created_at=current_time_in_seconds,
+            delete_id=delete_id,
+            email=email,
+            full_name=full_name,
+            id=user_id,
+            links=links,
+            user_name=user_name,
+        )
+
+        session.add_all([user])
+        session.commit()
+
+        return f"""
+            <div class='success'>
+                Account added. Delete ID: ({delete_id})
+            </div>
+            <p class='p-2 text-center'>
+                You social links are now available at
+                <a
+                    href='{request.base_url}u/{user.user_name}'
+                    target='_blank'
+                >
+                    {request.base_url}u/{user.user_name}
+                </a>
+            </p>
+        """
+
+
 def home_factory(db_engine: Engine, /):
     def home():
-        current_time_in_seconds = get_current_time_in_seconds()
-
         if request.method == "POST":
-            user_name = request.form["user_name"]
-            email = request.form["email"]
-            full_name = request.form["full_name"]
-            github = request.form["github"]
-            twitter = request.form["twitter"]
-            youtube = request.form["youtube"]
-            linkedin = request.form["linkedin"]
-            facebook = request.form["facebook"]
+            return _handle_post(db_engine)
 
-            if not user_name:
-                return '<div class="error">Username is required</div>'
-            elif not email:
-                return '<div class="error">Email is required</div>'
-            elif not full_name:
-                return '<div class="error">Your full name is required</div>'
-            elif (
-                not github
-                and not twitter
-                and not youtube
-                and not linkedin
-                and not facebook
-                and not youtube
-            ):
-                return '<div class="error">At least one social link is required</div>'
-            else:
-                with Session(db_engine) as session:
-                    user_id = str(uuid.uuid4())
-                    delete_id = str(uuid.uuid4())
-                    links = []
-                    if github:
-                        links.append(
-                            Link(
-                                id=str(uuid.uuid4()),
-                                user_id=user_id,
-                                website="github",
-                                link=github,
-                                created_at=current_time_in_seconds,
-                            )
-                        )
-                    if twitter:
-                        links.append(
-                            Link(
-                                id=str(uuid.uuid4()),
-                                user_id=user_id,
-                                website="twitter",
-                                link=twitter,
-                                created_at=current_time_in_seconds,
-                            )
-                        )
-                    if youtube:
-                        links.append(
-                            Link(
-                                id=str(uuid.uuid4()),
-                                user_id=user_id,
-                                website="youtube",
-                                link=youtube,
-                                created_at=current_time_in_seconds,
-                            )
-                        )
-                    if linkedin:
-                        links.append(
-                            Link(
-                                id=str(uuid.uuid4()),
-                                user_id=user_id,
-                                website="linkedin",
-                                link=linkedin,
-                                created_at=current_time_in_seconds,
-                            )
-                        )
-                    if facebook:
-                        links.append(
-                            Link(
-                                id=str(uuid.uuid4()),
-                                user_id=user_id,
-                                website="facebook",
-                                link=facebook,
-                                created_at=current_time_in_seconds,
-                            )
-                        )
-                    user = User(
-                        id=user_id,
-                        delete_id=delete_id,
-                        email=email,
-                        full_name=full_name,
-                        user_name=user_name,
-                        created_at=current_time_in_seconds,
-                        links=links,
-                    )
-                    session.add_all([user])
-                    session.commit()
-
-                    return f"""
-                        <div class='success'>
-                            Account added. Delete ID: ({delete_id})
-                        </div>
-                        <p class='p-2 text-center'>
-                            You social links are now available at
-                            <a href='{request.base_url}u/{user.user_name}' target='_blank'>
-                            {request.base_url}u/{user.user_name}
-                            </a>
-                        </p>
-                    """
-
-        return render_template("index.html", page_url=url_for("home"))
+        return render_template(
+            "index.html",
+            page_url=url_for("home"),
+        )
 
     return home
